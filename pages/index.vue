ef93871982e3dd4ddc77bff381d1cb19dc29c08f
messtt <corentin.albertus@epitech.eu>
2022-12-01 15:55:15 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mainBox">
         <p>Le <a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Notion</a></p>
-        <p>Documentation <a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a></p>
+        <p>Documentation <a href="https://github.com/messtt" target="_blank">link 3</a></p>
         <p>Documentation <a href="https://vuejs.org/" target="_blank">Vuejs</a></p>
     </div>
 </template>

06e1da949f39b0d5e0a243a9e44d8cdd3c8702c2
messtt <corentin.albertus@epitech.eu>
2022-12-01 17:34:58 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="mainBox">
-      <div class="btn"><a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Thomy</a></div>
-      <div class="btn"><a href="https://v3.nuxtjs.org/" target="_blank"></a> Nuxt 3</div>
-      <div class="btn"><a href="https://vuejs.org/" target="_blank"></a>Vuejs</div>
-      <div class="btn"><a href="https://github.com/messtt" target="_blank"></a>github</div>
+      <a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank"><div class="btn">Thomy</div></a>
+      <a href="https://v3.nuxtjs.org/" target="_blank"><div class="btn">Nuxt 3</div></a>
+      <a href="https://vuejs.org/" target="_blank"><div class="btn">Vuejs</div></a>
+      <a href="https://github.com/messtt" target="_blank"><div class="btn">github</div></a>
     </div>
 </template>
 
 <style scoped>
 .mainBox {
-    justify-content: space-between;
+    justify-content: space-around;
     background-color: #3D578C;
     display: flex;
     flex-direction: column;

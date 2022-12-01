4dc1ac5103c9356ebb7e4a4242c9b656b40e421b
messtt <corentin.albertus@epitech.eu>
2022-12-01 16:44:40 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,16 @@
         <p>Le <a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Notion</a></p>
         <button>Documentation <a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a></button>
         <button>Documentation <a href="https://vuejs.org/" target="_blank">Vuejs</a></button>
-        <button><a href="https://github.com/messtt" target="_blank">link_github</a></button>
+        <button><a href="https://github.com/messtt" target="_blank">github</a></button>
     </div>
 </template>
 
 <style scoped>
 
 .mainBox {
+    background-color: #3D578C;
     display: flex;
-    flex-direction: column;
+    flex-direction: v-bind();
     align-items: center;
     justify-content: center;
     width: 100vw;

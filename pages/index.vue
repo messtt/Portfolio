0f1653ccd195a7e05427a84c8c290a133dfca7d1
messtt <corentin.albertus@epitech.eu>
2022-12-01 17:07:47 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mainBox">
       <div class="btn"><p><a href="https://thomy-lorenzatti.notion.site/thomy-lorenzatti/Pr-sentation-JAM-Portfolio-1e8c57ecb4044871bb57198238500928" target="_blank">Thomy</a></p></div>
-      <div class="btn"><button><a href="https://v3.nuxtjs.org/" target="_blank">Nuxt 3</a></button></div>
+      <div class="btn"><button>href="https://v3.nuxtjs.org/" target="_blank" Nuxt 3</button></div>
       <div class="btn"><button><a href="https://vuejs.org/" target="_blank">Vuejs</a></button></div>
       <div class="btn"><button><a href="https://github.com/messtt" target="_blank">github</a></button></div>
     </div>
@@ -27,6 +27,14 @@
     margin: 0 60px;
     font-size: 20px;
     font-weight: 500;
+}
+
+a{
+  height: 60px;
+  width: 200px;
+  margin: 0 60px;
+  font-size: 20px;
+  font-weight: 500;
 }
 
 .btn:hover{

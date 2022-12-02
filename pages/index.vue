2c4d4f02391a9e3d199e5304c89867f044fdad71
messtt <corentin.albertus@epitech.eu>
2022-12-02 11:52:19 +0100

    -js

--- pages/index.vue
+++ pages/index.vue
@@ -83,17 +83,6 @@ a:hover{
   }
 }
 
-.background {
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  margin: 0;
-  padding: 0;
-  background: #3584e4;
-  overflow: hidden;
-}
 .background li {
   position: absolute;
   top: 80vh;

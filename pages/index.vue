1e2af4b15ad181baff17b3a2b9b80db68a668198
messtt <corentin.albertus@epitech.eu>
2022-12-02 11:04:08 +0100

    img

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="mainBox">
-      <div class="header"><h1>Mon site</h1></div>
       <div class="lien">
         <a href="https://portfolio-gamma-ashy-38.vercel.app/api/github">github</a>
         <a href="https://portfolio-gamma-ashy-38.vercel.app/api/github">github</a>
@@ -9,8 +8,96 @@
         <a href="https://portfolio-gamma-ashy-38.vercel.app/api/github">github</a>
       </div>
     </div>
+  <div class="container">
+    <div class="text"></div>
+  <script>
+    class TextScramble {
+      constructor(el) {
+        this.el = el;
+        this.chars = '!<>-_\\/[]{}—=+*^?#________';
+        this.update = this.update.bind(this);
+      }
+      setText(newText) {
+        const oldText = this.el.innerText;
+        const length = Math.max(oldText.length, newText.length);
+        const promise = new Promise(resolve => this.resolve = resolve);
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+          const from = oldText[i] || '';
+          const to = newText[i] || '';
+          const start = Math.floor(Math.random() * 40);
+          const end = start + Math.floor(Math.random() * 40);
+          this.queue.push({ from, to, start, end });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+      }
+      update() {
+        let output = '';
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+          let { from, to, start, end, char } = this.queue[i];
+          if (this.frame >= end) {
+            complete++;
+            output += to;
+          } else if (this.frame >= start) {
+            if (!char || Math.random() < 0.28) {
+              char = this.randomChar();
+              this.queue[i].char = char;
+            }
+            output += `<span class="dud">${char}</span>`;
+          } else {
+            output += from;
+          }
+        }
+        this.el.innerHTML = output;
+        if (complete === this.queue.length) {
+          this.resolve();
+        } else {
+          this.frameRequest = requestAnimationFrame(this.update);
+          this.frame++;
+        }
+      }
+      randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+      }}
+    const phrases = [
+      'Mon site',
+      'Le mien',
+      'Je l\'ai fait',
+      'C\'est a moi...',
+      'Messttt ;)',];
+    const el = document.querySelector('.text');
+    const fx = new TextScramble(el);
+    let counter = 0;
+    const next = () => {
+      fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800);
+      });
+      counter = (counter + 1) % phrases.length;
+    };
+    next();
+  </script>
+  </div>
 </template>
 <style scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.text {
+  font-weight: 100;
+  font-size: 28px;
+  color: #ffffff;
+}
+.dud {
+  color: #080303;
+}
 .mainBox {
     justify-content: space-around;
     background-color: #3D578C;
@@ -21,12 +108,12 @@
     height: 100vh;
 }
 
-.header{
+/*.header{
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 100px;
-}
+}*/
 
 .lien{
   display: flex;

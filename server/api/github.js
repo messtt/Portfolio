147e3a10a135ed25a48bf44be18054bfe7602e9d
messtt <corentin.albertus@epitech.eu>
2022-12-24 13:15:37 +0100

    test changement pour que sa fonctionne

--- server/api/github.js
+++ server/api/github.js
@@ -6,13 +6,12 @@ export default defineEventHandler(async (event) => {
 
     const { data } = await axios({
         method: 'get',
-        url: 'https://api.github.com/users/messtt/repos',
+        url: 'https://api.github.com/users/Shin-gema/repos',
         Headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
             'Accept-Encoding': 'identity',
         },
     })
-    console.log(data)
     return data
 })
--- server/api/hello.js
+++ server/api/hello.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
-export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig()
-    const instance = axios.create({
-        baseURL: "https://api.github.com",
-        headers: {
-            "Authorization": `Bearer ${config.token}`,
-            "Accept": "application/vnd.github+json",
-            "Accept-Encoding": "Identify"
-        },
-    });
-
-    const response =
-        await instance.get("/user/repos");
-    return response.data;
-});
+export default defineEventHandler(()=> {
+    return {
+        api: 'works'
+    }
+})
